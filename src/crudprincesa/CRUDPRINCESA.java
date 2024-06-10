acd7de8cc569ef3231441cafdbfd818de50a7e84
unknown <emmaprofemx@gmail.com>
2024-06-09 23:29:15 -0600

    primer commit

---
+++ src/crudprincesa/CRUDPRINCESA.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crudprincesa;
+
+import gui.Veterinaria;
+
+/**
+ *
+ * @author EMMANUEL
+ */
+public class CRUDPRINCESA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Veterinaria log = new Veterinaria();
+        log.show();
+    }
+    
+}
