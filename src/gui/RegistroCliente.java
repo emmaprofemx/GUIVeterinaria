fcda981e5734b2c91ae5b97e6bd4a3cb9ddfd3dd
unknown <emmaprofemx@gmail.com>
2024-06-17 23:36:04 -0600

    Ventana AddItems
    ventana para agregar articulos de mascotas

--- src/gui/RegistroCliente.java
+++ src/gui/RegistroCliente.java
@@ -365,13 +365,8 @@ public class RegistroCliente extends javax.swing.JFrame {
         Statement st = null;
         ResultSet rs = null;
         try {
-            // Cargar el driver JDBC de MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Establecer la conexión con la base de datos
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false", "root", "password");
-
-            // Crear la consulta SQL para obtener los datos
             String sql = "SELECT * FROM cliente";
             st = con.createStatement();
             rs = st.executeQuery(sql);
@@ -395,10 +390,7 @@ public class RegistroCliente extends javax.swing.JFrame {
     }
 
     private void loadInformacion(String sql, Statement st, ResultSet rs, DefaultTableModel tblModel) throws SQLException {
-        // Limpiar la tabla antes de agregar nuevos datos
         tblModel.setRowCount(0);
-
-        // Añadir filas de datos al modelo de tabla
         while (rs.next()) {
             String id = String.valueOf(rs.getInt("id"));
             String tnombre = rs.getString("nombre");
@@ -406,7 +398,6 @@ public class RegistroCliente extends javax.swing.JFrame {
             String ttelefono = rs.getString("telefono");
             String tcorreo = rs.getString("correo");
             String tdireccion = rs.getString("direccion");
-
             String tbData[] = {id, tnombre, tapellido, ttelefono, tcorreo, tdireccion};
             tblModel.addRow(tbData);
         }
